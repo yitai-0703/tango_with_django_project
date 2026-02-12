dbcf13bb0bfbe05309e793bc7ac790be732e5629
Yitai Lyu <3070833l@student.gla.ac.uk>
2026-02-12 15:39:54 +0000

    Fix chapter 3

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'rango/index.html')
+    return HttpResponse('Rango says hey there partner! <a href="/rango/about/">About</a>')
 
 def about(request):
-    return render(request, 'rango/about.html')
+    return HttpResponse('Rango says here is the about page. <a href="/rango/">Index</a>')
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rango import views 
-
+from rango import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rango/', views.index, name='index'),
-    path('rango/', include('rango.urls'), name='index'),
+    path('rango/', include('rango.urls')),
+    path('', views.index,name = 'index')
     
 ]
